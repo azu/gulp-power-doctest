621f5fa2f7121c2470c87f42390a857116ead5e6
azu <azuciao@gmail.com>
2013-12-24 23:17:22 +0900

    add error handling

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 var es = require("event-stream");
 var power = require("power-doctest");
+var gutil = require('gulp-util');
 module.exports = function (options) {
     options = options || {};
     return es.map(function (file, callback) {
         var contents = String(file.contents);
-        var results = power.runDocTest({
-            filePath: file.path,
-            fileData: contents
-        }, options);
+        if (!contents || contents.length === 0) {
+            return callback(null, file);
+        }
+        var results;
+        try {
+            results = power.runDocTest({
+                filePath: file.path,
+                fileData: contents
+            }, options);
+        } catch (e) {
+            gutil.log(e + " @ " + file.path);
+        }
         if (results && results.length > 0) {
             return callback(power.printTestResult(results), file);
         }
